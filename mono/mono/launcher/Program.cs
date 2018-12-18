9feccdc6d7f79fdaa7e6c6cafd5b07dba3185def
Chris Lewis <lewisc@pop-os.localdomain>
2018-12-18 15:41:24 -0700

    launcher testing

--- mono/mono/launcher/Program.cs
+++ mono/mono/launcher/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Gtk;
+using GameSparks.Core;
+using GameSparks.Api;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+using GameSparks.Api.Messages;
 
 namespace launcher
 {
@@ -7,6 +12,8 @@ namespace launcher
     {
         public static void Main(string[] args)
         {
+        
+
             Application.Init();
             MainWindow win = new MainWindow();
             win.Show();
