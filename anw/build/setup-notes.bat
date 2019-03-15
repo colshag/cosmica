648f53a250fb7fca3f36c56c3bcfa94e9158eb7a
Chris Lewis <colshag@gmail.com>
2019-03-15 17:06:21 -0600

    git

--- anw/build/setup-notes.bat
+++ anw/build/setup-notes.bat
@@ -1,17 +1,11 @@
 @echo off
 echo ================================================
-echo Welcome to the Cosmica Dependency Installer
-echo I will be attempting to install various python 
-echo dependencies using the python pip installer
-echo.
-echo First you need to install python
-echo make sure to enable the "Add python.exe to Path" option!
+echo Cosmica Dependency Installer
 echo ================================================
 echo.
-echo After that you will have to manually run the setup-dependencies.bat file
-echo This will install all the stuff you need to run cosmica!
-echo.
-echo Sorry for the hassle, at some point i'll make this better.
+echo First you need to install python
+echo ================================================
+echo >>> make sure to ENABLE the "Add python.exe to Path" option! <<<
 echo ================================================
 echo Chris Lewis.
 echo NeuroJump. 2019.
