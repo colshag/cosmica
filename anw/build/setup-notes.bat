712aec34063ad4a01dff74bbc0fc40a277e5a90d
Chris Lewis <colshag@gmail.com>
2019-03-15 17:43:38 -0600

    .

--- anw/build/setup-notes.bat
+++ anw/build/setup-notes.bat
@@ -2,10 +2,9 @@
 echo ================================================
 echo Cosmica Dependency Installer
 echo ================================================
-echo.
 echo First you need to install python
 echo ================================================
-echo >>> make sure to ENABLE the "Add python.exe to Path" option! <<<
+echo make sure to ENABLE the "Add python.exe to Path" option!
 echo ================================================
 echo Chris Lewis.
 echo NeuroJump. 2019.
